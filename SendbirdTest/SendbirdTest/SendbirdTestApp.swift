a321d4395c6bfd89d142a187f5a3e55ad9a87b84
dortus47 <dortus47@naver.com>
2022-10-29 18:55:26 +0900

    프로젝트 추가

---
+++ SendbirdTest/SendbirdTest/SendbirdTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendbirdTestApp.swift
+//  SendbirdTest
+//
+//  Created by 장은석 on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct SendbirdTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
